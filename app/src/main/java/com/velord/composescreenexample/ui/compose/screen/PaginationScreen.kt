23f5228a5053bf81558b0414c7a995e16cb596c4
Velord <vlad.yaroshenko>
2023-02-17 18:01:26 +0200

    Add: PaginationScreen skeleton

---
+++ app/src/main/java/com/velord/composescreenexample/ui/compose/screen/PaginationScreen.kt
@@ -0,0 +1,20 @@
+package com.velord.composescreenexample.ui.compose.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PaginationScreen() {
+
+}
+
+@Composable
+private fun Content() {
+
+}
+
+@Preview
+@Composable
+private fun PaginationContentPreview() {
+    Content()
+}
