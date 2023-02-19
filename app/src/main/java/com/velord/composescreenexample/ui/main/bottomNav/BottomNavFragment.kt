7315ffe123cfcdc6fbd5af5dfe6bf069da7b0007
Velord <vlad.yaroshenko>
2023-02-19 19:45:20 +0200

    Add: BottomNavFragment

--- app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/BottomNavFragment.kt
+++ app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/BottomNavFragment.kt
@@ -1,9 +1,11 @@
 package com.velord.composescreenexample.ui.main.bottomNav
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,13 +16,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import coil.compose.AsyncImage
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.velord.composescreenexample.ui.compose.theme.GunPowder
 import com.velord.composescreenexample.ui.compose.theme.RegularAmethystSmoke14Style
 import com.velord.composescreenexample.ui.compose.theme.setContentWithTheme
+import com.velord.model.utils.dpToPx
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -42,10 +50,20 @@ class BottomNavFragment : Fragment() {
 
 @Composable
 private fun BottomNavScreen(viewModel: BottomNavViewModel) {
+    val configuration = LocalConfiguration.current
+    val density = LocalDensity.current
+    val screenWidthPx = with(density) {configuration.screenWidthDp.dp.roundToPx() }
+    val screenHeightPx = with(density) {configuration.screenHeightDp.dp.roundToPx() }
+    AsyncImage(
+        model = "https://picsum.photos/seed/BottomNavScreen/$screenWidthPx/$screenHeightPx",
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.FillBounds,
+    )
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.background)
             .statusBarsPadding()
     ) {
         Text(
