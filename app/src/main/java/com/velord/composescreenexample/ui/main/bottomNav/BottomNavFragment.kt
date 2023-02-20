2a8300bb7c0033da64a3003ee9741ccccc501cdf
Velord <vlad.yaroshenko>
2023-02-20 17:07:33 +0200

    Add: Camera API

--- app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/BottomNavFragment.kt
+++ app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/BottomNavFragment.kt
@@ -5,42 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.video.*
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
+import com.velord.composescreenexample.R
 import com.velord.composescreenexample.ui.compose.component.FullSizeBackground
+import com.velord.composescreenexample.ui.compose.theme.GunPowder
 import com.velord.composescreenexample.ui.compose.theme.RegularAmethystSmoke14Style
+import com.velord.composescreenexample.ui.compose.theme.SteelGray
 import com.velord.composescreenexample.ui.compose.theme.setContentWithTheme
 import com.velord.composescreenexample.ui.compose.utils.getScreenWidthAndHeightInPx
 import com.velord.composescreenexample.utils.PermissionState
-import com.velord.composescreenexample.utils.context.createSettingsIntent
+import com.velord.composescreenexample.utils.context.*
 import com.velord.composescreenexample.utils.fragment.checkRecordVideoPermission
 import com.velord.composescreenexample.utils.fragment.viewLifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class BottomNavFragment : Fragment() {
 
-    private val navController by lazy {
-        childFragmentManager.fragments.first().findNavController()
-    }
     private val viewModel by viewModels<BottomNavViewModel>()
     private val recordVideoViewModel by viewModels<RecordVideoViewModel>()
 
@@ -50,7 +58,6 @@ class BottomNavFragment : Fragment() {
         val areGranted = it.values.reduce { acc, next -> acc && next }
         recordVideoViewModel.updatePermissionState(PermissionState.invoke(areGranted))
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -103,37 +110,225 @@ private fun BottomNavScreen(
     viewModel: BottomNavViewModel,
     recordVideoViewModel: RecordVideoViewModel
 ) {
-    val (screenWidthPx, screenHeightPx) = getScreenWidthAndHeightInPx()
-    val url = "https://picsum.photos/seed/BottomNavScreen/$screenWidthPx/$screenHeightPx"
-    FullSizeBackground(url = url)
-
     val permissionState = recordVideoViewModel.permissionFlow.collectAsStateWithLifecycle()
+    val qualityState = recordVideoViewModel.videoQualityFlow.collectAsStateWithLifecycle()
+    val cameraSelectorState = recordVideoViewModel.videoCameraSelectorFlow.collectAsStateWithLifecycle()
+    val isAudioEnabledState = recordVideoViewModel.videoIsAudioEnabledFlow.collectAsStateWithLifecycle()
 
     Content(
-        state = permissionState.value,
-        onClick = recordVideoViewModel::onCheckPermission
+        permission = permissionState.value,
+        quality = qualityState.value,
+        cameraSelector = cameraSelectorState.value,
+        isAudioEnabled = isAudioEnabledState.value,
+        fileMetaData = recordVideoViewModel.createFileMetadata(),
+        onCheckPermissionClick = recordVideoViewModel::onCheckPermission,
+        onChangeCameraSelector = recordVideoViewModel::onChangeVideoCameraSelector,
+        onVideoRecordEvent = recordVideoViewModel::onVideoRecordEvent,
+        onNewRecording = recordVideoViewModel::onNewRecording,
+        onStopRecording = recordVideoViewModel::onStopRecording
     )
 }
 
 @Composable
 private fun Content(
-    state: PermissionState,
-    onClick: () -> Unit
+    permission: PermissionState,
+    quality: Quality,
+    cameraSelector: CameraSelector,
+    isAudioEnabled: Boolean,
+    fileMetaData: RecordVideoMetaData,
+    onCheckPermissionClick: () -> Unit,
+    onChangeCameraSelector: () -> Unit,
+    onVideoRecordEvent: (VideoRecordEvent) -> Unit,
+    onNewRecording: (Recording) -> Unit,
+    onStopRecording: () -> Unit
 ) {
-    if (state.isDenied()) {
+    if (permission.isDenied()) {
+        val (screenWidthPx, screenHeightPx) = getScreenWidthAndHeightInPx()
+        val url = "https://picsum.photos/seed/BottomNavScreen/$screenWidthPx/$screenHeightPx"
+        FullSizeBackground(url)
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .statusBarsPadding()
         ) {
             Text(
-                text = "Check permission",
+                text = stringResource(id = R.string.can_not_get_permission),
                 modifier = Modifier
                     .align(Alignment.Center)
                     .padding(16.dp)
                     .background(Color.White)
-                    .clickable { onClick() },
+                    .clickable { onCheckPermissionClick() },
                 style = RegularAmethystSmoke14Style
+            )
+        }
+    } else {
+        CameraRecordingPreview(
+            quality = quality,
+            cameraSelector = cameraSelector,
+            isAudioEnabled = isAudioEnabled,
+            fileMetaData = fileMetaData,
+            onChangeCameraSelector = onChangeCameraSelector,
+            onVideoRecordEvent = onVideoRecordEvent,
+            onNewRecording = onNewRecording,
+            onStopRecording = onStopRecording
+        )
+    }
+}
+
+@Composable
+private fun CameraRecordingPreview(
+    quality: Quality,
+    cameraSelector: CameraSelector,
+    isAudioEnabled: Boolean,
+    fileMetaData: RecordVideoMetaData,
+    onChangeCameraSelector: () -> Unit,
+    onVideoRecordEvent: (VideoRecordEvent) -> Unit,
+    onNewRecording: (Recording) -> Unit,
+    onStopRecording: () -> Unit
+) {
+    val context = LocalContext.current
+    // PreviewView is a custom view that will display the camera feed.
+    // We will bind it to the lifecycle, add it to the AndroidView
+    // and it will show us what we are currently recording.
+    val previewView = remember { PreviewView(context) }
+    // VideoCapture is a generic class that provides a camera stream suitable for video applications.
+    // Here we pass the Recorder class which is an implementation of the VideoOutput interface
+    // and it allows us to start recording.
+    val videoCaptureState: MutableState<VideoCapture<Recorder>?> = remember { mutableStateOf(null) }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(cameraSelector) {
+        videoCaptureState.value = context.createVideoCapture(
+            lifecycleOwner = lifecycleOwner,
+            cameraSelector = cameraSelector,
+            previewView = previewView,
+            preferredQuality = quality
+        )
+    }
+
+    AndroidView(
+        factory = { previewView },
+        modifier = Modifier.fillMaxSize()
+    )
+
+    Adjustments(
+        isAudioEnabled = isAudioEnabled,
+        fileMetaData = fileMetaData,
+        videoCapture = videoCaptureState.value,
+        onChangeCameraSelector = onChangeCameraSelector,
+        onVideoRecordEvent = onVideoRecordEvent,
+        onNewRecording = onNewRecording,
+        onStopRecording = onStopRecording
+    )
+}
+
+@Composable
+private fun Adjustments(
+    isAudioEnabled: Boolean,
+    fileMetaData: RecordVideoMetaData,
+    videoCapture: VideoCapture<Recorder>?,
+    onChangeCameraSelector: () -> Unit,
+    onVideoRecordEvent: (VideoRecordEvent) -> Unit,
+    onNewRecording: (Recording) -> Unit,
+    onStopRecording: () -> Unit
+) {
+    val isRecordingStartedState = remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+    ) {
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            StartStop(
+                isAudioEnabled = isAudioEnabled,
+                fileMetaData = fileMetaData,
+                videoCapture = videoCapture,
+                isRecordingStarted = isRecordingStartedState.value,
+                onChangeIsRecordingStarted = {
+                    isRecordingStartedState.value = it
+                    if (it.not()) {
+                        onStopRecording()
+                    }
+                },
+                onNewRecording = onNewRecording,
+                onEvent = onVideoRecordEvent
+            )
+            CameraSelector(
+                isRecordingStarted = isRecordingStartedState.value,
+                onChangeCameraSelector = onChangeCameraSelector
+            )
+        }
+    }
+}
+
+@Composable
+private fun StartStop(
+    isAudioEnabled: Boolean,
+    fileMetaData: RecordVideoMetaData,
+    videoCapture: VideoCapture<Recorder>?,
+    isRecordingStarted: Boolean,
+    onChangeIsRecordingStarted: (Boolean) -> Unit,
+    onNewRecording: (Recording) -> Unit,
+    onEvent: (VideoRecordEvent) -> Unit
+) {
+    val context = LocalContext.current
+
+    IconButton(
+        onClick = {
+            if (isRecordingStarted.not()) {
+                videoCapture?.let {
+                    onChangeIsRecordingStarted(true)
+
+                    val recording = context.createRecording(
+                        fileMetaData = fileMetaData,
+                        videoCapture = videoCapture,
+                        audioEnabled = isAudioEnabled,
+                    ) { event ->
+                        onEvent(event)
+                    }
+
+                    onNewRecording(recording)
+                }
+            } else {
+                onChangeIsRecordingStarted(false)
+            }
+        }
+    ) {
+        val icon =  Icons.Filled.run {
+            if (isRecordingStarted) Stop else AddPhotoAlternate
+        }
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(64.dp),
+            tint = Color.SteelGray
+        )
+    }
+}
+
+@Composable
+private fun CameraSelector(
+    isRecordingStarted: Boolean,
+    onChangeCameraSelector: () -> Unit
+) {
+    if (isRecordingStarted.not()) {
+        IconButton(
+            onClick = onChangeCameraSelector,
+            modifier = Modifier
+        ) {
+            Icon(
+                imageVector = Icons.Filled.SwitchVideo,
+                contentDescription = null,
+                modifier = Modifier.size(64.dp),
+                tint = Color.SteelGray
             )
         }
     }
--- app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/RecordVideoViewModel.kt
+++ app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/RecordVideoViewModel.kt
@@ -1,30 +1,114 @@
 package com.velord.composescreenexample.ui.main.bottomNav
 
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.video.*
 import com.velord.composescreenexample.utils.PermissionState
+import com.velord.composescreenexample.utils.context.createDirInCache
 import com.velord.composescreenexample.viewModel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
+
+private const val DIR_NAME = "video"
+private const val VIDEO_FILE_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
+data class RecordVideoMetaData(
+    val outputDirectory: File,
+    val fileNameFormat: String,
+)
 
 @HiltViewModel
 class RecordVideoViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
 ) : BaseViewModel() {
-
+    // Permission
     val permissionFlow = MutableStateFlow(PermissionState.NotAsked)
+    // User interaction
     val goToSettingsEvent = MutableSharedFlow<Unit>()
     val checkPermissionEvent = MutableSharedFlow<Unit>()
-
-    fun updatePermissionState(state: PermissionState) {
-        permissionFlow.value = state
-    }
+    // Adjustments
+    val videoQualityFlow = MutableStateFlow(Quality.HIGHEST)
+    val videoCameraSelectorFlow = MutableStateFlow(CameraSelector.DEFAULT_FRONT_CAMERA)
+    val videoIsAudioEnabledFlow = MutableStateFlow(true)
+    // Video control
+    // A Recording is an object that allows us to control current active recording.
+    // It will allow us to stop, pause and resume the current recording.
+    // We create that object when we start recording.
+    val recordingFlow: MutableStateFlow<Recording?> = MutableStateFlow(null)
 
     fun onGoToSettingsClick() = launch {
         goToSettingsEvent.emit(Unit)
     }
 
+    fun updatePermissionState(state: PermissionState) {
+        permissionFlow.value = state
+    }
+
+
     fun onCheckPermission() = launch {
         checkPermissionEvent.emit(Unit)
+    }
+
+    fun onChangeVideoQuality(quality: Quality) {
+        videoQualityFlow.value = quality
+    }
+
+    fun onChangeVideoCameraSelector() {
+        val isBackCamera = videoCameraSelectorFlow.value == CameraSelector.DEFAULT_BACK_CAMERA
+        val newCamera = if (isBackCamera) {
+            CameraSelector.DEFAULT_FRONT_CAMERA
+        } else {
+            CameraSelector.DEFAULT_BACK_CAMERA
+        }
+        videoCameraSelectorFlow.value = newCamera
+    }
+
+    fun onChangeVideoIsAudioEnabled(enabled: Boolean) {
+        videoIsAudioEnabledFlow.value = enabled
+    }
+
+    fun onNewRecording(recording: Recording) {
+        recordingFlow.value = recording
+    }
+
+    fun onStopRecording() {
+        recordingFlow.value?.stop()
+    }
+
+    fun onVideoRecordEvent(newEvent: VideoRecordEvent) {
+        Log.d("@@@", "Recording event: $newEvent")
+        if (newEvent is VideoRecordEvent.Finalize) {
+            val uri = newEvent.outputResults.outputUri
+            //if (uri != Uri.EMPTY) saveVideo(uri)
+            Log.d("@@@", "Recording uri: $uri")
+        }
+    }
+
+    fun createFileMetadata(): RecordVideoMetaData {
+        val mediaDir = context.createDirInCache(DIR_NAME)
+        val dir = if (mediaDir != null && mediaDir.exists()) mediaDir else context.filesDir
+        return RecordVideoMetaData(
+            outputDirectory = dir,
+            fileNameFormat = VIDEO_FILE_FORMAT,
+        )
+    }
+
+    private fun saveVideoToGallery(uri: Uri) {
+        val values = ContentValues(1).apply {
+            put(MediaStore.Video.Media.TITLE, "My video title")
+            put(MediaStore.Video.Media.MIME_TYPE, "video/mp4")
+            put(MediaStore.Video.Media.DATA, uri.path)
+        }
+        val newUri = context.contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values)
+
     }
 }
