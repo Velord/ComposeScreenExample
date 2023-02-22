78d39d7588ea735b6f9a13adf0185ac40e843b34
Velord <vlad.yaroshenko>
2023-02-22 04:45:51 +0200

    Add: BottomNavFragment content

--- app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/BottomNavFragment.kt
+++ app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/BottomNavFragment.kt
@@ -1,85 +1,188 @@
 package com.velord.composescreenexample.ui.main.bottomNav
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.velord.composescreenexample.R
+import com.velord.composescreenexample.databinding.FragmentBottomNavBinding
+import com.velord.composescreenexample.ui.compose.preview.PreviewCombined
+import com.velord.composescreenexample.ui.compose.theme.ClearRippleTheme
 import com.velord.composescreenexample.ui.compose.theme.setContentWithTheme
-import com.velord.composescreenexample.utils.PermissionState
-import com.velord.composescreenexample.utils.context.*
-import com.velord.composescreenexample.utils.fragment.checkRecordVideoPermission
 import com.velord.composescreenexample.utils.fragment.viewLifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class BottomNavFragment : Fragment() {
+class BottomNavFragment : Fragment(R.layout.fragment_bottom_nav) {
 
-    private val viewModel by viewModels<BottomNavViewModel>()
-    private val cameraRecordingViewModel by viewModels<CameraRecordingViewModel>()
-
-    private val requestRecordVideoPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) {
-        val areGranted = it.values.reduce { acc, next -> acc && next }
-        cameraRecordingViewModel.updatePermissionState(PermissionState.invoke(areGranted))
+    private val navController by lazy {
+        childFragmentManager.fragments.first().findNavController()
     }
+    private val viewModel by viewModels<BottomNavViewModel>()
+    private var binding: FragmentBottomNavBinding? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = setContentWithTheme {
-        BottomnNavScreen(viewModel)
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentBottomNavBinding.bind(view).apply {
+            initView()
+        }
         initObserving()
+    }
+
+    context(FragmentBottomNavBinding)
+    private fun initView() {
+        bottomNavBarView.setContentWithTheme {
+            BottomNavScreen(viewModel)
+        }
     }
 
     private fun initObserving() {
         viewLifecycleScope.launch {
-            launch {
-                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                    checkRecordVideoPermission()
-                }
-            }
-            launch {
-                cameraRecordingViewModel.goToSettingsEvent.collect {
-                    startActivity(requireContext().createSettingsIntent())
-                }
-            }
-            launch {
-                cameraRecordingViewModel.checkPermissionEvent.collect {
-                    checkRecordVideoPermission()
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tabFlow.collectLatest {
+                    navController.navigate(it.navigationId)
                 }
             }
         }
     }
+}
 
-    private fun checkRecordVideoPermission() {
-        checkRecordVideoPermission(
-            actionLauncher = requestRecordVideoPermissionLauncher,
-            onGranted = {
-                cameraRecordingViewModel.updatePermissionState(PermissionState.Granted)
-            },
-            onDecline = {
-                cameraRecordingViewModel.updatePermissionState(PermissionState.Denied)
-            }
+@Composable
+private fun BottomNavScreen(viewModel: BottomNavViewModel) {
+    val tabFlow = viewModel.tabFlow.collectAsStateWithLifecycle()
+
+    Content(
+        selectedItem = tabFlow.value,
+        onClick = viewModel::onTabClick,
+    )
+}
+
+@Composable
+fun AnimatableIcon(
+    text: String,
+    imageVector: ImageVector,
+    scale: Float,
+    color: Color,
+    modifier: Modifier = Modifier,
+    iconSize: Dp = 64.dp,
+    animateDuration: Int = 500,
+    onClick: () -> Unit = {}
+) {
+    val animatedScale: Float by animateFloatAsState(
+        targetValue = scale,
+        animationSpec = TweenSpec(
+            durationMillis = animateDuration,
+            easing = FastOutSlowInEasing
+        )
+    )
+    val animatedColor by animateColorAsState(
+        targetValue = color,
+        animationSpec = TweenSpec(
+            durationMillis = animateDuration,
+            easing = FastOutSlowInEasing
+        )
+    )
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = animatedColor,
+            modifier = modifier
+                .scale(animatedScale)
+                .size(iconSize)
+        )
+        Text(
+            text = text,
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.labelSmall
         )
     }
 }
 
 @Composable
-private fun BottomnNavScreen(
-    viewModel: BottomNavViewModel,
+private fun Content(
+    selectedItem: BottomNavItem,
+    onClick: (BottomNavItem) -> Unit,
 ) {
+    NavigationBar(
+        modifier = Modifier.navigationBarsPadding().height(72.dp),
+    ) {
+        BottomNavItem.values().forEach {
+            val isSelected = selectedItem == it
+            NavigationBarItem(
+                selected = isSelected,
+                onClick = { onClick(it) },
+                label = {},
+                icon = {
+                    val color = MaterialTheme.colorScheme.run {
+                        if (isSelected) secondary else onSurface
+                    }
+                    AnimatableIcon(
+                        text = it.name,
+                        imageVector = it.icon,
+                        scale = if (isSelected) 1.5f else 1f,
+                        color = color,
+                        modifier = Modifier,
+                        iconSize = 28.dp,
+                    ) {
+                        onClick(it)
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                        LocalAbsoluteTonalElevation.current
+                    )
+                )
+            )
+        }
+    }
+}
 
+@PreviewCombined
+@Composable
+private fun BottomNavContentPreview() {
+    Content(
+        selectedItem = BottomNavItem.Camera,
+        onClick = {},
+    )
 }
