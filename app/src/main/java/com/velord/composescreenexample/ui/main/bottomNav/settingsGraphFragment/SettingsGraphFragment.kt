7213761907e4910f085866d2438c0221ecc59ff5
Velord <vlad.yaroshenko>
2023-03-07 01:17:31 +0200

    Add: graphs to bottom_nav_graph.xml

--- app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/settingsGraphFragment/SettingsGraphFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.velord.composescreenexample.ui.main.bottomNav.settingsGraphFragment
-
-import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class SettingsGraphFragment : Fragment()
