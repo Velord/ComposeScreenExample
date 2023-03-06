b47b4f9a99a09281daa488035f0194d09bbad59a
Velord <vlad.yaroshenko>
2023-03-07 01:04:41 +0200

    Add: test graphs

---
+++ app/src/main/java/com/velord/composescreenexample/ui/main/bottomNav/cameraGraphFragment/CameraGraphFragment.kt
@@ -0,0 +1,8 @@
+package com.velord.composescreenexample.ui.main.bottomNav.cameraGraphFragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CameraGraphFragment : Fragment() {
+}
--- app/src/main/java/com/velord/composescreenexample/ui/main/inDevelopment/InDevelopmentFragment.kt
+++ app/src/main/java/com/velord/composescreenexample/ui/main/inDevelopment/InDevelopmentFragment.kt
@@ -1,12 +1,7 @@
 package com.velord.composescreenexample.ui.main.inDevelopment
 
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.velord.composescreenexample.ui.main.bottomNav.CameraRecordingViewModel
-import com.velord.composescreenexample.utils.PermissionState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class InDevelopmentFragment : Fragment() {
-}
+class InDevelopmentFragment : Fragment()
