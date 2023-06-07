6a7a2339d819bd997f62a53eac9455e832716449
Velord <vlad.yaroshenko>
2023-06-07 03:23:52 +0200

    Add: shapes and paths

--- uicore/src/main/java/com/velord/uicore/compose/Shape.kt
+++ uicore/src/main/java/com/velord/uicore/compose/Shape.kt
@@ -1,8 +1,12 @@
 package com.velord.uicore.compose
 
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import com.velord.uicore.compose.path.createTicketPath
+import com.velord.uicore.compose.path.pervasiveArcFromBottomPath
 
 val MainShapes = Shapes(
     extraSmall = RoundedCornerShape(4.dp),
@@ -11,3 +15,15 @@ val MainShapes = Shapes(
     large = RoundedCornerShape(16.dp),
     extraLarge = RoundedCornerShape(24.dp)
 )
+
+fun TicketShape(cornerRadius: Float): Shape =
+    GenericShape { size, _ ->
+        addPath(createTicketPath(cornerRadius, size))
+    }
+
+fun PervasiveArcFromBottomShape(
+    progress: Int,
+    onEdgeTouch: () -> Unit
+) : Shape = GenericShape { size, _ ->
+    addPath(pervasiveArcFromBottomPath(size, progress, onEdgeTouch))
+}
