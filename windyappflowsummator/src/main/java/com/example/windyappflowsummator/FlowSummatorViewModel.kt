6966fa36c116fd05add988d3d3768c9eec8cd569
Velord <vlad.yaroshenko.work@gmail.com>
2023-07-07 18:16:25 +0300

    Refactor: launching logic

--- windyappflowsummator/src/main/java/com/example/windyappflowsummator/FlowSummatorViewModel.kt
+++ windyappflowsummator/src/main/java/com/example/windyappflowsummator/FlowSummatorViewModel.kt
@@ -1,14 +1,14 @@
 package com.example.windyappflowsummator
 
-import android.util.Log
 import com.velord.util.viewModel.BaseViewModel
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -23,22 +23,24 @@ class FlowSummatorViewModel : BaseViewModel() {
 
     val currentTextFlow: MutableStateFlow<String> = MutableStateFlow("")
     val currentEnteredNumberFlow: MutableStateFlow<Int?> = MutableStateFlow(null)
-    private val emitNumberFlow: MutableSharedFlow<EmitNumber> = MutableSharedFlow(
+    private val sumFlow: MutableSharedFlow<EmitNumber> = MutableSharedFlow(
         replay = 1,
         extraBufferCapacity = 10,
         onBufferOverflow = BufferOverflow.SUSPEND
     )
+    private val launchSumFlow: MutableSharedFlow<Int> = MutableSharedFlow()
 
     init {
-        observeEmitNumberFlow()
+        observeSumFlow()
+        observeSumLaunchFlow()
     }
 
-    fun onStartClick() {
-        if (currentEnteredNumberFlow.value == null) return // Case when user didn't enter anything
-        if (currentEnteredNumberFlow.value == 0) return // Case when user entered 0
+    fun onStartClick() = launch {
+        // Case when user didn't enter anything
+        if (currentEnteredNumberFlow.value == null) return@launch
 
         currentEnteredNumberFlow.value?.let {
-            launchSummator(it)
+            launchSumFlow.emit(it)
         }
     }
 
@@ -46,7 +48,7 @@ class FlowSummatorViewModel : BaseViewModel() {
         val calculatedValue = if (newValue.toIntOrNull() == null) {
             // Case when user entered not a number(from clipboard framework)
             // or empty string
-            // or that is not a number(BigInteger in that case)
+            // or that is not a number(Int in that case)
             if (newValue.isEmpty()) null // Case when user cleared the field
             else currentEnteredNumberFlow.value
         } else {
@@ -56,31 +58,8 @@ class FlowSummatorViewModel : BaseViewModel() {
         currentEnteredNumberFlow.value = calculatedValue
     }
 
-    private fun launchSummator(value: Int) = launch {
-        currentTextFlow.value = ""
-        emitNumberFlow.resetReplayCache()
-
-        val flows = createFlows(value)
-        // Результирующий Flow должен суммировать значения всех N Flow.
-        flows.forEach { flow ->
-            launch {
-                // В задаче flow эмитит только 1 значение -> take(1)
-                flow.take(1).collect { newValue ->
-                    val sdfds = getPrevEmittedValue()
-                    Log.d("!!", "getPrevEmittedValue: $sdfds")
-                    Log.d("!!", "newValue: $newValue")
-                    val number = EmitNumber(
-                        previousValue = getPrevEmittedValue(),
-                        newValue = newValue
-                    )
-                    emitNumberFlow.emit(number)
-                }
-            }
-        }
-    }
-
     private fun getPrevEmittedValue(): Int =
-        emitNumberFlow.replayCache.firstOrNull()?.sum ?: 0
+        sumFlow.replayCache.firstOrNull()?.sum ?: 0
 
     private fun createFlows(countOfFlowToCreate: Int): Array<Flow<Int>> {
         val flows = mutableListOf<Flow<Int>>()
@@ -88,8 +67,8 @@ class FlowSummatorViewModel : BaseViewModel() {
         repeat(countOfFlowToCreate) { index ->
             flows += flow {
                 // после задержки в (index + 1) * 100
-                val delay = (index + 1) * 100L
-                delay(delay)
+                val waitFor = (index + 1) * 100L
+                delay(waitFor)
                 // эмитит значение index + 1
                 emit(index + 1)
             }
@@ -97,11 +76,35 @@ class FlowSummatorViewModel : BaseViewModel() {
         return flows.toTypedArray()
     }
 
-    private fun observeEmitNumberFlow() = launch {
-        emitNumberFlow.collect { newNumber ->
+    private fun observeSumFlow() = launch {
+        sumFlow.collect { newNumber ->
             // Суммирующий Flow должен возвращать значение после обновления каждого из N Flow.
             currentTextFlow.update {
                 it + "\n" + newNumber.sum
+            }
+        }
+    }
+
+    private fun observeSumLaunchFlow() = launch {
+        launchSumFlow.collectLatest { flowCount ->
+            // Reset state
+            currentTextFlow.value = ""
+            sumFlow.resetReplayCache()
+
+            val flows = createFlows(flowCount)
+            // Результирующий Flow должен суммировать значения всех N Flow.
+            coroutineScope {
+                flows.forEach { flow ->
+                    launch {
+                        flow.collect { newNumber ->
+                            val number = EmitNumber(
+                                previousValue = getPrevEmittedValue(),
+                                newValue = newNumber
+                            )
+                            sumFlow.emit(number)
+                        }
+                    }
+                }
             }
         }
     }
