d7e87a8ad62e05f37a60c0479a9cb03d6ac5b68b
Velord <vlad.yaroshenko.work@gmail.com>
2023-07-07 16:51:17 +0300

    Add: EnterField and Start logic

--- windyappflowsummator/src/main/java/com/example/windyappflowsummator/FlowSummatorViewModel.kt
+++ windyappflowsummator/src/main/java/com/example/windyappflowsummator/FlowSummatorViewModel.kt
@@ -6,8 +6,27 @@ import kotlinx.coroutines.flow.MutableStateFlow
 class FlowSummatorViewModel : BaseViewModel() {
 
     val state: MutableStateFlow<Int> = MutableStateFlow(0)
+    val currentEnteredValueFlow: MutableStateFlow<String?> = MutableStateFlow(null)
 
     fun onEvent() {
 
+    }
+
+    fun onStartClick() {
+
+    }
+
+    fun onNewEnteredValue(newValue: String) {
+        val calculatedValue = if (newValue.toIntOrNull() == null) {
+            // Case when user entered not a number(from clipboard framework)
+            // or empty string
+            // or that is not a number(BigInteger)
+            if (newValue.isEmpty()) null // Case when user cleared the field
+            else currentEnteredValueFlow.value
+        } else {
+            newValue
+        }
+
+        currentEnteredValueFlow.value = calculatedValue
     }
 }
