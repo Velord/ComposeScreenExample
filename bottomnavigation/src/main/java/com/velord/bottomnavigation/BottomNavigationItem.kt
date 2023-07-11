0206b94475e9b12b6526d9db79f33a77dd81b54c
Velord <vlad.yaroshenko.work@gmail.com>
2023-07-11 14:06:58 +0300

    Rename: center bottom navigation item

--- bottomnavigation/src/main/java/com/velord/bottomnavigation/BottomNavigationItem.kt
+++ bottomnavigation/src/main/java/com/velord/bottomnavigation/BottomNavigationItem.kt
@@ -1,8 +1,8 @@
 package com.velord.bottomnavigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Camera
+import androidx.compose.material.icons.outlined.Hexagon
 import androidx.compose.material.icons.outlined.Settings
 import com.velord.multiplebackstackapplier.MultipleBackstackGraphItem
 
@@ -11,13 +11,13 @@ enum class BottomNavigationItem(
     override val startDestinationId: Int
 ) : MultipleBackstackGraphItem {
     Camera(R.id.left_nav_graph, R.id.bottom_nav_graph_left_start_destination),
-    Add(R.id.center_nav_graph, R.id.bottom_nav_graph_center_start_destination),
+    Demo(R.id.center_nav_graph, R.id.bottom_nav_graph_center_start_destination),
     Settings(R.id.right_nav_graph, R.id.bottom_nav_graph_right_start_destination);
 
 
     val icon get() = when (this) {
         Camera -> Icons.Outlined.Camera
-        Add -> Icons.Outlined.Add
+        Demo -> Icons.Outlined.Hexagon
         Settings -> Icons.Outlined.Settings
     }
 }
