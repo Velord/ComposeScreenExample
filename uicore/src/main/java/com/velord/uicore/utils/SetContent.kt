8bd56d7a9a6ea9d2033d2322a0397520abe700bd
Velord <vlad.yaroshenko.work@gmail.com>
2023-07-12 20:58:01 +0300

    Add: retrieve SettingsViewModel from Activity

--- uicore/src/main/java/com/velord/uicore/utils/SetContent.kt
+++ uicore/src/main/java/com/velord/uicore/utils/SetContent.kt
@@ -7,43 +7,40 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.velord.uicore.compose.theme.MainTheme
-import kotlinx.coroutines.flow.Flow
+import com.velord.util.context.getActivity
 
 fun ComponentActivity.setContentWithTheme(
-    themeSwitcher: Flow<ThemeSwitcher>? = null,
     screen: @Composable ComposeView.() -> Unit
-): ComposeView = ComposeView(this).setContentWithTheme(
-    themeSwitcher = themeSwitcher,
-    screen = screen
-)
+): ComposeView = ComposeView(this).setContentWithTheme(screen)
 
 fun Fragment.setContentWithTheme(
-    themeSwitcher: Flow<ThemeSwitcher>? = null,
     screen: @Composable ComposeView.() -> Unit
-): ComposeView = ComposeView(requireContext()).setContentWithTheme(
-    themeSwitcher = themeSwitcher,
-    screen = screen
-)
+): ComposeView = ComposeView(requireContext()).setContentWithTheme(screen)
 
 context(LifecycleOwner)
 @SuppressLint("UnrememberedMutableState")
 fun ComposeView.setContentWithTheme(
-    themeSwitcher: Flow<ThemeSwitcher>? = null,
     screen: @Composable ComposeView.() -> Unit
 ): ComposeView = apply {
     setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
     setContent {
-        val themeSwitcherState: State<ThemeSwitcher> = themeSwitcher
-            ?.collectAsStateWithLifecycle(ThemeSwitcher.considerSystem())
-            ?: mutableStateOf(ThemeSwitcher.considerSystem())
+        val activity = LocalContext.current.getActivity()
+        val settingsViewModel = viewModel<SettingsViewModel>(
+            viewModelStoreOwner = activity as ViewModelStoreOwner
+        )
+        val themeSwitcherState: State<ThemeSwitcher> = settingsViewModel.themeSwitcherFlow
+            .collectAsStateWithLifecycle(ThemeSwitcher.considerSystem())
+
         Log.d("@@@", "setContentWithTheme: ${themeSwitcherState.value}")
 
         CompositionLocalProvider(LocalThemeSwitcher provides themeSwitcherState.value) {
