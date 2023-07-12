07b74e27987019d23af97717457b86080f9f6c02
Velord <vlad.yaroshenko.work@gmail.com>
2023-07-12 18:21:01 +0300

    Add: light theme

--- uicore/src/main/java/com/velord/uicore/compose/theme/color/Color.kt
+++ uicore/src/main/java/com/velord/uicore/compose/theme/color/Color.kt
@@ -2,41 +2,81 @@ package com.velord.uicore.compose.theme.color
 
 import androidx.compose.ui.graphics.Color
 
+// Dark
 // Primary
 val Color.Companion.DarkPrimary: Color get() = Color(0xFFc5c0ff)
-val Color.Companion.DarkOnPrimary: Color get() = Color(0xFF2b2276)
+val Color.Companion.DarkOnPrimary: Color get() = Color(0xFF2b2277)
 val Color.Companion.DarkPrimaryContainer: Color get() = Color(0xFF423b8e)
-val Color.Companion.DarkOnPrimaryContainer: Color get() = Color(0xFFe4dfff)
-val Color.Companion.DarkInversePrimary: Color get() = Color(0xFFc5c0ff) // need to do
+val Color.Companion.DarkOnPrimaryContainer: Color get() = Color(0xFFe3dfff)
+val Color.Companion.DarkInversePrimary: Color get() = Color(0xFF5A54A8)
 // Secondary
-val Color.Companion.DarkSecondary: Color get() = Color(0xFFc7bfff)
-val Color.Companion.DarkOnSecondary: Color get() = Color(0xFF2e2176)
-val Color.Companion.DarkSecondaryContainer: Color get() = Color(0xFF453a8e)
-val Color.Companion.DarkOnSecondaryContainer: Color get() = Color(0xFFe4dfff)
+val Color.Companion.DarkSecondary: Color get() = Color(0xFFe9b3ff)
+val Color.Companion.DarkOnSecondary: Color get() = Color(0xFF500670)
+val Color.Companion.DarkSecondaryContainer: Color get() = Color(0xFF692789)
+val Color.Companion.DarkOnSecondaryContainer: Color get() = Color(0xFFf7d8ff)
 // Tertiary
-val Color.Companion.DarkTertiary: Color get() = Color(0xFFd4bbff)
-val Color.Companion.DarkOnTertiary: Color get() = Color(0xFF41008b)
-val Color.Companion.DarkTertiaryContainer: Color get() = Color(0xFF5d00c2)
-val Color.Companion.DarkOnTertiaryContainer: Color get() = Color(0xFFebdcff)
+val Color.Companion.DarkTertiary: Color get() = Color(0xFF58d6f7)
+val Color.Companion.DarkOnTertiary: Color get() = Color(0xFF003641)
+val Color.Companion.DarkTertiaryContainer: Color get() = Color(0xFF004e5e)
+val Color.Companion.DarkOnTertiaryContainer: Color get() = Color(0xFFb1ecff)
 // Background
-val Color.Companion.DarkBackground: Color get() = Color(0xFF1c1b1f)
-val Color.Companion.DarkOnBackground: Color get() = Color(0xFFe5e1e6)
+val Color.Companion.DarkBackground: Color get() = Color(0xFF001f2a)
+val Color.Companion.DarkOnBackground: Color get() = Color(0xFFbfe9ff)
 // Surface
-val Color.Companion.DarkSurface: Color get() = Color(0xFF1c1b1f)
-val Color.Companion.DarkOnSurface: Color get() = Color(0xFFe5e1e6)
+val Color.Companion.DarkSurface: Color get() = Color(0xFF001f2a)
+val Color.Companion.DarkOnSurface: Color get() = Color(0xFFbfe9ff)
 val Color.Companion.DarkSurfaceVariant: Color get() = Color(0xFF47464f)
 val Color.Companion.DarkOnSurfaceVariant: Color get() = Color(0xFFc8c5d0)
-val Color.Companion.DarkSurfaceTint: Color get() = Color(0xFF2b2a2e) // need to do
-val Color.Companion.DarkInverseSurface: Color get() = Color(0xFF1c1b1f) // need to do
-val Color.Companion.DarkOnInverseSurface: Color get() = Color(0xFFe5e1e6) // need to do
+val Color.Companion.DarkSurfaceTint: Color get() = Color(0xFFC5C0FF)
+val Color.Companion.DarkInverseSurface: Color get() = Color(0xFFBFE9FF)
+val Color.Companion.DarkInverseOnSurface: Color get() = Color(0xFF001F2A)
 // Error
-val Color.Companion.DarkError: Color get() = Color(0xFFff8c8c)
-val Color.Companion.DarkOnError: Color get() = Color(0xFF5F0505)
-val Color.Companion.DarkErrorContainer: Color get() = Color(0xFFff8c8c)
-val Color.Companion.DarkOnErrorContainer: Color get() = Color(0xFFCF0909)
+val Color.Companion.DarkError: Color get() = Color(0xFFffb4ab)
+val Color.Companion.DarkOnError: Color get() = Color(0xFF690005)
+val Color.Companion.DarkErrorContainer: Color get() = Color(0xFF93000a)
+val Color.Companion.DarkOnErrorContainer: Color get() = Color(0xFFffdad6)
 // Outline
-val Color.Companion.DarkOutline: Color get() = Color(0xFF928f99) // need to do
-val Color.Companion.DarkOutlineVariant: Color get() = Color(0xFF928f99) // need to do
+val Color.Companion.DarkOutline: Color get() = Color(0xFF928f99)
+val Color.Companion.DarkOutlineVariant: Color get() = Color(0xFF47464F)
 // Scrim
-val Color.Companion.DarkScrim: Color get() = Color(0x7f000000) // need to do
+val Color.Companion.DarkScrim: Color get() = Color(0x7f000000)
+
+// Light
+// Primary
+val Color.Companion.LightPrimary: Color get() = Color(0xFF5a54a8)
+val Color.Companion.LightOnPrimary: Color get() = Color(0xFFffffff)
+val Color.Companion.LightPrimaryContainer: Color get() = Color(0xFFe3dfff)
+val Color.Companion.LightOnPrimaryContainer: Color get() = Color(0xFF140463)
+val Color.Companion.LightInversePrimary: Color get() = Color(0xFFC5C0FF)
+// Secondary
+val Color.Companion.LightSecondary: Color get() = Color(0xFF8341a3)
+val Color.Companion.LightOnSecondary: Color get() = Color(0xFFffffff)
+val Color.Companion.LightSecondaryContainer: Color get() = Color(0xFFf7d8ff)
+val Color.Companion.LightOnSecondaryContainer: Color get() = Color(0xFF310048)
+// Tertiary
+val Color.Companion.LightTertiary: Color get() = Color(0xFF00677c)
+val Color.Companion.LightOnTertiary: Color get() = Color(0xFFffffff)
+val Color.Companion.LightTertiaryContainer: Color get() = Color(0xFFb1ecff)
+val Color.Companion.LightOnTertiaryContainer: Color get() = Color(0xFF001f27)
+// Background
+val Color.Companion.LightBackground: Color get() = Color(0xFFfafcff)
+val Color.Companion.LightOnBackground: Color get() = Color(0xFF001f2a)
+// Surface
+val Color.Companion.LightSurface: Color get() = Color(0xFFfafcff)
+val Color.Companion.LightOnSurface: Color get() = Color(0xFF001f2a)
+val Color.Companion.LightSurfaceVariant: Color get() = Color(0xFFe5e1ec)
+val Color.Companion.LightOnSurfaceVariant: Color get() = Color(0xFF47464f)
+val Color.Companion.LightSurfaceTint: Color get() = Color(0xFF5A54A8)
+val Color.Companion.LightInverseSurface: Color get() = Color(0xFF003547)
+val Color.Companion.LightInverseOnSurface: Color get() = Color(0xFFE1F4FF)
+// Error
+val Color.Companion.LightError: Color get() = Color(0xFFba1a1a)
+val Color.Companion.LightOnError: Color get() = Color(0xFFffffff)
+val Color.Companion.LightErrorContainer: Color get() = Color(0xFFffdad6)
+val Color.Companion.LightOnErrorContainer: Color get() = Color(0xFF410002)
+// Outline
+val Color.Companion.LightOutline: Color get() = Color(0xFF787680)
+val Color.Companion.LightOutlineVariant: Color get() = Color(0xFFC8C5D0)
+// Scrim
+val Color.Companion.LightScrim: Color get() = Color(0xFF000000)
 
