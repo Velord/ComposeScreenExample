c18ff8a2aba7d32ae6e6d29ff29cf0cf7fbfb1fc
Velord <vlad.yaroshenko.work@gmail.com>
2024-01-05 17:38:52 +0200

    Update: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,10 +160,3 @@ dependencies {
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
     kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
-
-// https://slack-chats.kotlinlang.org/t/9025044/after-updating-my-project-to-kotlin-1-8-0-i-m-getting-the-fo
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = libs.versions.jvmTarget.get()
-    }
-}
