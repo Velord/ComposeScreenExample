65e772bbe246ef1febb17e9efd36db33012d8e36
Velord <vlad.yaroshenko>
2024-03-30 03:23:04 +0200

    Add: AppState

--- data/datastore/src/main/java/com/velord/datastore/DataStoreService.kt
+++ data/datastore/src/main/java/com/velord/datastore/DataStoreService.kt
@@ -1,6 +1,7 @@
 package com.velord.datastore
 
 import com.velord.datastore.appSettings.AppSettingsDataStore
+import com.velord.util.settings.AppSettings
 import com.velord.util.settings.ThemeConfig
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
@@ -10,21 +11,21 @@ import org.koin.core.annotation.Single
 interface DataStoreService {
     suspend fun checkAppFirstLaunch(): Boolean
     suspend fun setThemeConfig(theme: ThemeConfig)
-    suspend fun getThemeConfigFlow(): Flow<ThemeConfig>
+    suspend fun getAppSettingsFlow(): Flow<AppSettings>
 }
 
 @Single
 class DataStoreServiceImpl(
-    private val dataStore: AppSettingsDataStore
+    private val settings: AppSettingsDataStore
 ) : DataStoreService {
 
     private suspend fun setFirstLaunch() {
-        dataStore.updateData {
+        settings.updateData {
             it.copy(isAppFirstLaunch = true)
         }
     }
 
-    override suspend fun checkAppFirstLaunch(): Boolean = dataStore.data.map {
+    override suspend fun checkAppFirstLaunch(): Boolean = settings.data.map {
         val isFirstLaunch = it.isAppFirstLaunch
         if (isFirstLaunch) setFirstLaunch()
 
@@ -32,10 +33,10 @@ class DataStoreServiceImpl(
     }.first()
 
     override suspend fun setThemeConfig(theme: ThemeConfig) {
-       dataStore.updateData {
+       settings.updateData {
            it.copy(theme = theme)
        }
     }
 
-    override suspend fun getThemeConfigFlow(): Flow<ThemeConfig> = dataStore.data.map { it.theme }
+    override suspend fun getAppSettingsFlow(): Flow<AppSettings> = settings.data
 }
