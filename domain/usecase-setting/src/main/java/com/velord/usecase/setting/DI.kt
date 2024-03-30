4d1fda20ba00f97317899e38f6bc967eaf96049e
Velord <vlad.yaroshenko>
2024-03-30 02:58:42 +0200

    Add: GetThemeGateway

--- domain/usecase-setting/src/main/java/com/velord/usecase/setting/DI.kt
+++ domain/usecase-setting/src/main/java/com/velord/usecase/setting/DI.kt
@@ -1,8 +1,5 @@
 package com.velord.usecase.setting
 
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
-
-@Module
-@ComponentScan("com.velord.usecase.setting")
-class SettingUseCaseModule
+//@Module
+//@ComponentScan("com.velord.usecase.setting")
+//class SettingUseCaseModule
