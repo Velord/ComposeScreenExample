d71582522900e4e29df74dbcf9e690d6b3de4a53
Velord <vlad.yaroshenko>
2024-03-30 03:33:17 +0200

    Add: module appstate

--- data/gatewaysetting/src/main/java/com/velord/gateway/setting/GetThemeConfigGateway.kt
+++ data/gatewaysetting/src/main/java/com/velord/gateway/setting/GetThemeConfigGateway.kt
@@ -1,26 +1,17 @@
 package com.velord.gateway.setting
 
+import com.velord.appstate.AppStateService
 import com.velord.datastore.DataStoreService
 import com.velord.usecase.setting.GetThemeConfigDS
 import com.velord.util.settings.ThemeConfig
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import org.koin.core.annotation.Single
 
-// Lets imagine it is another data store in different module
-interface AppState {
-    val flow: MutableStateFlow<ThemeConfig>
-}
-@Single
-class AppStateImpl : AppState {
-    override val flow = MutableStateFlow(ThemeConfig.DEFAULT)
-}
-
 @Single
 class GetThemeConfigGateway(
     private val dataStore: DataStoreService,
-    private val appState: AppState
+    private val appState: AppStateService
 ) : GetThemeConfigDS {
 
     private var isInitialized = false
