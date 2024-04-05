3e83a7c8c70baf18422e1d9008ff07d946664366
Velord <vladik.yaroshenko@yandex.ru>
2024-04-05 16:34:31 +0300

    Update: screens of bottom navigation

--- ui/feature-bottomnavigation/src/main/java/com/velord/bottomnavigation/BottomNavigationGraph.kt
+++ ui/feature-bottomnavigation/src/main/java/com/velord/bottomnavigation/BottomNavigationGraph.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.ExternalModuleGraph
 import com.ramcosta.composedestinations.annotation.NavGraph
+import com.velord.bottomnavigation.screen.BottomNavigationVoyagerScreen
+import com.velord.bottomnavigation.viewmodel.BottomNavViewModelVoyager
 import org.koin.androidx.compose.koinViewModel
 
 private const val BOTTOM_NAVIGATION_GRAPH = "bottom_navigation_graph"
@@ -18,6 +20,6 @@ internal annotation class BottomNavigationGraph
 @Destination<BottomNavigationGraph>(start = true)
 @Composable
 internal fun BottomNavigationDestination() {
-    val viewModel = koinViewModel<BottomNavViewModelJetpack>()
-    BottomNavScreen(viewModel)
+    val viewModel = koinViewModel<BottomNavViewModelVoyager>()
+    BottomNavigationVoyagerScreen(viewModel)
 }
