6bb8921ddd84f4e700ab8ffcbba9460c923647ed
Velord <vlad.yaroshenko.work@gmail.com>
2024-07-22 15:33:24 +0300

    Add: GetMovieSortOptionUCTest

--- domain/usecase-movie/src/main/java/com/velord/usecase/movie/GetMovieSortOptionUC.kt
+++ domain/usecase-movie/src/main/java/com/velord/usecase/movie/GetMovieSortOptionUC.kt
@@ -3,7 +3,8 @@ package com.velord.usecase.movie
 import com.velord.model.movie.MovieSortOption
 import com.velord.usecase.movie.dataSource.MovieSortDS
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 
 class GetMovieSortOptionUC(private val movieSortDS: MovieSortDS) {
-    operator fun invoke(): Flow<List<MovieSortOption>> = movieSortDS.getFlow()
+    operator fun invoke(): Flow<List<MovieSortOption>> = movieSortDS.getFlow().catch {}
 }
