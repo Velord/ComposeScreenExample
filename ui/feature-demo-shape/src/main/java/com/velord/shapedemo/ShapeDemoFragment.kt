dca37e1370e6084f5f188eb667dbea85ede30d13
Velord <vlad.yaroshenko.work@gmail.com>
2024-11-26 16:56:28 +0200

    Refactor: ShapeDemo

--- ui/feature-demo-shape/src/main/java/com/velord/shapedemo/ShapeDemoFragment.kt
+++ ui/feature-demo-shape/src/main/java/com/velord/shapedemo/ShapeDemoFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -14,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.velord.uicore.utils.setContentWithTheme
 
@@ -45,6 +48,8 @@ private fun Content() {
         TicketShapeDemo()
         PervasiveArcFromBottomShapeDemo()
         WaveShapeDemo()
+
+        Spacer(modifier = Modifier.size(32.dp))
     }
 }
 
