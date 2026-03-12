0b616104e3f8d20c8b8839fff8ff795ff620b22b
Velord <vlad.yaroshenko.work@gmail.com>
2026-03-12 23:55:45 +0200

    Delete: unused files

--- util/src/main/java/com/velord/util/settings/AppSettings.kt
+++
@@ -1,9 +0,0 @@
-package com.velord.util.settings
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AppSettings(
-    val isAppFirstLaunch: Boolean = false,
-    val theme: ThemeConfig = ThemeConfig.DEFAULT
-)
