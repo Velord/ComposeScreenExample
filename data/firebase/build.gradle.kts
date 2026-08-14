9fda91b49e38a8ac84b02615d634a0a940bee75c
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-14 19:42:41 +0300

    Add: Firebase Remote Config data source module

---
+++ data/firebase/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.convention.multiplatform.library)
+    alias(libs.plugins.ksp)
+}
+
+kotlin {
+    android {
+        namespace = "com.velord.data.firebase"
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            // Koin
+            implementation(libs.koin.core)
+            api(libs.koin.annotation)
+        }
+        androidMain.dependencies {
+            // Firebase
+            implementation(libs.gitlive.firebase.config)
+        }
+    }
+
+    sourceSets.named("commonMain").configure {
+        kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
+    }
+}
+
+dependencies {
+    // KSP
+    add("kspCommonMainMetadata", libs.koin.ksp)
+    add("kspAndroid", libs.koin.ksp)
+    add("kspDesktop", libs.koin.ksp)
+}
+
+tasks.matching {
+    it.name.startsWith("ksp") &&
+        it.name != "kspCommonMainKotlinMetadata"
+}.configureEach {
+    dependsOn("kspCommonMainKotlinMetadata")
+}
