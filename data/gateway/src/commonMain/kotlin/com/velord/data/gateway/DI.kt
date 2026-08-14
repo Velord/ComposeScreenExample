c991205023e9a40cccbaa15c142926d38e1f7477
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-14 19:44:48 +0300

    Update: include Firebase data module

--- data/gateway/src/commonMain/kotlin/com/velord/data/gateway/DI.kt
+++ data/gateway/src/commonMain/kotlin/com/velord/data/gateway/DI.kt
@@ -6,6 +6,7 @@ import com.velord.data.appstate.AppStateModule
 import com.velord.data.backend.ktor.BackendModule
 import com.velord.data.datastore.DataStoreModule
 import com.velord.data.db.DbModule
+import com.velord.data.firebase.FirebaseModule
 import com.velord.data.os.OsModule
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
@@ -16,6 +17,7 @@ import org.koin.core.annotation.Module
         DbModule::class,
         BackendModule::class,
         DataStoreModule::class,
+        FirebaseModule::class,
         OsModule::class,
     ],
 )
