acec9be62ccb117294dd06ce36fc76ee152aa46e
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-19 00:30:56 +0300

    Cleanup: remove split Android localization platform hook

--- data/localization/src/androidMain/kotlin/com/velord/data/localization/RemoteConfigPlatform.kt
+++
@@ -1,7 +0,0 @@
-package com.velord.data.localization
-
-/**
- * FirebaseInitProvider initializes the default Android FirebaseApp before Application.onCreate.
- * The common Remote Config client can therefore use the native default app directly.
- */
-internal actual suspend fun initializeRemoteConfigPlatform() = Unit
