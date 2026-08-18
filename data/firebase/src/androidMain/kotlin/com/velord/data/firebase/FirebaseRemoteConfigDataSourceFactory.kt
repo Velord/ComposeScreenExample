0ac83a6073790aab5df84eb1c6a678affa6fc6b0
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 20:13:35 +0300

    Add: Android GitLive Remote Config implementation

---
+++ data/firebase/src/androidMain/kotlin/com/velord/data/firebase/FirebaseRemoteConfigDataSourceFactory.kt
@@ -0,0 +1,31 @@
+package com.velord.data.firebase
+
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.remoteconfig.remoteConfig
+
+private const val LOCALIZATION_PARAMETER = "localization"
+
+internal actual fun createFirebaseRemoteConfigDataSource(): FirebaseRemoteConfigDataSource =
+    AndroidFirebaseRemoteConfigDataSource()
+
+private class AndroidFirebaseRemoteConfigDataSource : FirebaseRemoteConfigDataSource {
+
+    private val remoteConfig
+        get() = Firebase.remoteConfig
+
+    override suspend fun initialize(defaultLocalization: String) {
+        remoteConfig.setDefaults(
+            LOCALIZATION_PARAMETER to defaultLocalization,
+        )
+        remoteConfig.ensureInitialized()
+    }
+
+    override fun getLocalization(): String? = remoteConfig
+        .getValue(LOCALIZATION_PARAMETER)
+        .asString()
+        .takeIf(String::isNotBlank)
+
+    override suspend fun fetchAndActivate() {
+        remoteConfig.fetchAndActivate()
+    }
+}
