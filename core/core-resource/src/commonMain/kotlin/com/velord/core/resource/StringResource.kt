1edb3b2681c5cb8b618d7dd91796f2caa42fa69c
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-19 00:18:31 +0300

    Move localization resource API into core-resource

---
+++ core/core-resource/src/commonMain/kotlin/com/velord/core/resource/StringResource.kt
@@ -0,0 +1,56 @@
+package com.velord.core.resource
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.velord.model.localization.LanguageCode
+import com.velord.model.localization.LocalizationState
+
+private val FORMAT_ARGUMENT_REGEX = Regex("""%([0-9]+)[$]([sd])""")
+
+val LocalLocalizationState = staticCompositionLocalOf<LocalizationState?> { null }
+
+@Composable
+fun stringResource(
+    resource: AppStringResource,
+    vararg formatArgs: Any,
+): String = getString(
+    localization = requireNotNull(LocalLocalizationState.current) {
+        "Localization is not provided"
+    },
+    resource = resource,
+    formatArgs = formatArgs,
+)
+
+fun getString(
+    localization: LocalizationState,
+    resource: AppStringResource,
+    vararg formatArgs: Any,
+): String = getString(
+    localization = localization,
+    language = localization.language,
+    resource = resource,
+    formatArgs = formatArgs,
+)
+
+fun getString(
+    localization: LocalizationState,
+    language: LanguageCode,
+    resource: AppStringResource,
+    vararg formatArgs: Any,
+): String {
+    val template = localization.document.languages[language]?.get(resource.key)
+        ?: defaultLocalizationStringRoster[resource.key]
+        ?: resource.key
+    return format(template, formatArgs)
+}
+
+private fun format(
+    template: String,
+    formatArgs: Array<out Any>,
+): String = FORMAT_ARGUMENT_REGEX.replace(template) { match ->
+    val argumentIndex = match.groupValues[1].toInt() - 1
+    require(argumentIndex in formatArgs.indices) {
+        "Missing format argument ${argumentIndex + 1} for: $template"
+    }
+    formatArgs[argumentIndex].toString()
+}
