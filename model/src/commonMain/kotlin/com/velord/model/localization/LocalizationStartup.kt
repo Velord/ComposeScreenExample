b207e6730340cb689eea37e9f3f87b8c210f6d83
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 20:55:51 +0300

    Refactor: remove obsolete localization startup model

--- model/src/commonMain/kotlin/com/velord/model/localization/LocalizationStartup.kt
+++
@@ -1,8 +0,0 @@
-package com.velord.model.localization
-
-import com.velord.model.setting.LanguagePreference
-
-data class LocalizationStartup(
-    val remoteJson: String?,
-    val languagePreference: LanguagePreference,
-)
