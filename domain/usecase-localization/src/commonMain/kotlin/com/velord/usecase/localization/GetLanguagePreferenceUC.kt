d1c63c9f1032011870f8041082321cf33845e780
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 20:55:12 +0300

    Refactor: remove standalone localization usecase module

--- domain/usecase-localization/src/commonMain/kotlin/com/velord/usecase/localization/GetLanguagePreferenceUC.kt
+++
@@ -1,6 +0,0 @@
-package com.velord.usecase.localization
-
-import com.velord.model.setting.LanguagePreference
-import kotlinx.coroutines.flow.Flow
-
-fun interface GetLanguagePreferenceUC : suspend () -> Flow<LanguagePreference>
