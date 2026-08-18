cf605c112201b425929bcf0e70bd6156e43730ee
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 22:41:54 +0300

    Refactor: fetch localization through common Firebase KMP API

--- data/localization/src/commonMain/kotlin/com/velord/data/localization/LocalizationRemoteDataSource.kt
+++ data/localization/src/commonMain/kotlin/com/velord/data/localization/LocalizationRemoteDataSource.kt
@@ -1,7 +1,7 @@
 package com.velord.data.localization
 
-import dev.gitlive.firebase.Firebase
-import dev.gitlive.firebase.remoteconfig.remoteConfig
+import com.firebasekit.core.Firebase
+import com.firebasekit.remoteconfig.remoteConfig
 import org.koin.core.annotation.Single
 
 private const val LOCALIZATION_PARAMETER = "localization"
@@ -9,21 +9,11 @@ private const val LOCALIZATION_PARAMETER = "localization"
 @Single
 class LocalizationRemoteDataSource {
 
-    private val remoteConfig get() = Firebase.remoteConfig
-
-    suspend fun initialize(defaultLocalization: String) {
-        remoteConfig.setDefaults(
-            LOCALIZATION_PARAMETER to defaultLocalization,
-        )
-        remoteConfig.ensureInitialized()
-    }
-
-    fun getLocalization(): String? = remoteConfig
-        .getValue(LOCALIZATION_PARAMETER)
-        .asString()
-        .takeIf(String::isNotBlank)
-
-    suspend fun fetchAndActivate() {
-        remoteConfig.fetchAndActivate()
+    suspend fun fetchLocalization(): String? {
+        initializeRemoteConfigPlatform()
+        Firebase.remoteConfig.fetchAndActivate()
+        return Firebase.remoteConfig
+            .getString(LOCALIZATION_PARAMETER)
+            ?.takeIf(String::isNotBlank)
     }
 }
