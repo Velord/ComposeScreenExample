5be1e00aff9d6ff3d09d30cffb07431de14120b3
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-19 00:13:11 +0300

    Refactor localization document model

---
+++ model/src/commonMain/kotlin/com/velord/model/localization/LocalizationDocument.kt
@@ -0,0 +1,14 @@
+package com.velord.model.localization
+
+@JvmInline
+value class LocalizationStrings(
+    val value: Map<String, String>,
+) {
+    operator fun get(key: String): String? = value[key]
+    val keys: Set<String> get() = value.keys
+}
+
+data class LocalizationDocument(
+    val schemaVersion: Int,
+    val languages: Map<LanguageCode, LocalizationStrings>,
+)
