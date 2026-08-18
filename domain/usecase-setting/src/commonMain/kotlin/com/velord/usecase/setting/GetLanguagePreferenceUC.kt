b5099bb8daa189fe144f04cd9fd090bf552111c4
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-19 01:06:17 +0300

    Refactor: remove duplicate language preference read use case

--- domain/usecase-setting/src/commonMain/kotlin/com/velord/usecase/setting/GetLanguagePreferenceUC.kt
+++
@@ -1,6 +0,0 @@
-package com.velord.usecase.setting
-
-import com.velord.model.setting.LanguagePreference
-import kotlinx.coroutines.flow.Flow
-
-fun interface GetLanguagePreferenceUC : suspend () -> Flow<LanguagePreference>
