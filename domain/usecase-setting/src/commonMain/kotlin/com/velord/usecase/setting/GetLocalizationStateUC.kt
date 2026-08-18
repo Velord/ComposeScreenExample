8c3eb44a842b1eb6107157949ee30e846291333f
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-19 00:20:41 +0300

    Add localization AppState use case

---
+++ domain/usecase-setting/src/commonMain/kotlin/com/velord/usecase/setting/GetLocalizationStateUC.kt
@@ -0,0 +1,6 @@
+package com.velord.usecase.setting
+
+import com.velord.model.localization.LocalizationState
+import kotlinx.coroutines.flow.StateFlow
+
+fun interface GetLocalizationStateUC : () -> StateFlow<LocalizationState?>
