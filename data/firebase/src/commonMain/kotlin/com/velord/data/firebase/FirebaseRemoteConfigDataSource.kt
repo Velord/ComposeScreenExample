0be46de3a73aa3f5860c61f66e55ebf71ca4a4e0
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 22:13:35 +0300

    Refactor: remove obsolete Firebase data source

--- data/firebase/src/commonMain/kotlin/com/velord/data/firebase/FirebaseRemoteConfigDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.velord.data.firebase
-
-interface FirebaseRemoteConfigDataSource {
-    suspend fun initialize(defaultLocalization: String)
-    fun getLocalization(): String?
-    suspend fun fetchAndActivate()
-}
