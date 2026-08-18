31eaba41a45a3052bd90a801adb23fddab2f641a
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 20:53:10 +0300

    Refactor: initialize localization from Android app startup

--- app/android/src/main/kotlin/com/velord/composescreenexample/App.kt
+++ app/android/src/main/kotlin/com/velord/composescreenexample/App.kt
@@ -5,22 +5,42 @@ import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.os.StrictMode.VmPolicy
 import com.velord.data.os.memory.MemoryLeakMonitor
+import com.velord.usecase.setting.FetchLocalizationUC
+import com.velord.usecase.setting.InitializeLocalizationUC
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.android.inject
 
 class App : Application() {
 
+    private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
     private val memoryLeakMonitor: MemoryLeakMonitor by inject()
+    private val initializeLocalizationUC: InitializeLocalizationUC by inject()
+    private val fetchLocalizationUC: FetchLocalizationUC by inject()
 
     override fun onCreate() {
         super.onCreate()
 
         initKoin()
+        initLocalization()
         initStrictMode()
         initMemoryLeakMonitor()
     }
 
     private fun initKoin() {
         startKoin()
+    }
+
+    private fun initLocalization() {
+        runBlocking {
+            initializeLocalizationUC()
+        }
+        appScope.launch {
+            fetchLocalizationUC()
+        }
     }
 
     private fun initStrictMode() {
