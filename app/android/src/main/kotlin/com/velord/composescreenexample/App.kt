95cb944f1f144fe03dd8135df63f96af765f478a
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 22:43:52 +0300

    Refactor: initialize localization once at app startup

--- app/android/src/main/kotlin/com/velord/composescreenexample/App.kt
+++ app/android/src/main/kotlin/com/velord/composescreenexample/App.kt
@@ -5,21 +5,14 @@ import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.os.StrictMode.VmPolicy
 import com.velord.data.os.memory.MemoryLeakMonitor
-import com.velord.usecase.setting.FetchLocalizationUC
 import com.velord.usecase.setting.InitializeLocalizationUC
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.android.inject
 
 class App : Application() {
 
-    private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
     private val memoryLeakMonitor: MemoryLeakMonitor by inject()
     private val initializeLocalizationUC: InitializeLocalizationUC by inject()
-    private val fetchLocalizationUC: FetchLocalizationUC by inject()
 
     override fun onCreate() {
         super.onCreate()
@@ -37,9 +30,6 @@ class App : Application() {
     private fun initLocalization() {
         runBlocking {
             initializeLocalizationUC()
-        }
-        appScope.launch {
-            fetchLocalizationUC()
         }
     }
 
