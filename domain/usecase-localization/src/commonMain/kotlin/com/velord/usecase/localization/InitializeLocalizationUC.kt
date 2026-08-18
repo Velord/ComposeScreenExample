56133faa42deb76e409973f8720651b600c7a708
Velord <vlad.yaroshenko.work@gmail.com>
2026-08-18 20:55:21 +0300

    Refactor: remove standalone localization usecase module

--- domain/usecase-localization/src/commonMain/kotlin/com/velord/usecase/localization/InitializeLocalizationUC.kt
+++
@@ -1,5 +0,0 @@
-package com.velord.usecase.localization
-
-import com.velord.model.localization.LocalizationStartup
-
-fun interface InitializeLocalizationUC : suspend (String) -> LocalizationStartup
